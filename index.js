e6048800120a7a9ee7bdde83a2ff3bfb42648db0
Pratham Kumar <Pratham.Kumar@winjittechnologiespvtltd.local>
2025-11-24 14:47:24 +0530

    add code in index.js file

---
+++ index.js
@@ -0,0 +1,13 @@
+// Simple JavaScript example
+
+// Function to add two numbers
+function add(a, b) {
+  return a + b;
+}
+
+// Main program
+const num1 = 5;
+const num2 = 7;
+
+console.log("Hello, World!");
+console.log(`${num1} + ${num2} = ${add(num1, num2)}`);
