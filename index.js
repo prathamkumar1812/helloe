cae3e0afbf2742769aaa40b59923b4c7c822a057
Pratham Kumar <Pratham.Kumar@winjittechnologiespvtltd.local>
2025-11-24 14:49:41 +0530

    change in index.js file

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function add(a, b) {
 const num1 = 5;
 const num2 = 7;
 
-console.log("Hello, World!");
-console.log(`${num1} + ${num2} = ${add(num1, num2)}`);
+console                               .log("Hello, World!");
+console                      .log(`${num1} + ${num2} = ${add(num1, num2)}`);
